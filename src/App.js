5e8ec0174582a0d33c29ba8543d70ec91765ac60
Agust Rinaldo Kurniawan <agust.kurniawan@Agust-Rinaldo-Kurniawan.local>
2021-05-11 15:13:45 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [image, setImage] = useState();
+
+  const onSubmit = () => {
+    var bodyFormData = new FormData();
+    bodyFormData.append("avatar", image);
+    axios
+      .post(url, bodyFormData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((r) => console.log({ response: r.data }))
+      .catch((e) => console.log({ error: e }));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={
+            image
+              ? URL.createObjectURL(image)
+              : "https://pixinvent.com/materialize-material-design-admin-template/app-assets/images/user/12.jpg"
+          }
+          alt="Avatar"
+          style={{
+            backgroundColor: "gray",
+            width: 100,
+            height: 100,
+            marginBottom: 10,
+          }}
+        />
+        <div style={{ flexDirection: "row" }}>
+          <input
+            type="file"
+            alt="avatar choose"
+            className="Button"
+            onChange={(e) => {
+              // console.log(URL.createObjectURL(e.target.files[0]));
+              setImage(e.target.files[0]);
+            }}
+          />
+          <button className="Button" onClick={onSubmit}>
+            Submit
+          </button>
+        </div>
+        <div style={{ flexDirection: "row" }}>
+          <input
+            type="text"
+            style={{ width: 200, marginTop: 10 }}
+            placeholder="api post url"
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              alert("url changed to : " + url);
+            }}
+          >
+            save url
+          </button>
+        </div>
       </header>
     </div>
   );
